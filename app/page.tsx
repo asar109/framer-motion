af69d131508e33f7dcdbb06a7aede6d0e49e5475
asar109 <themernist@gmail.com>
2024-02-28 21:39:34 +0500

    animation: 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Box1 from "@/components/box1";
 import Box2 from "@/components/box2";
+import Box3 from "@/components/box3";
 const page = () => {
   return (
     <div>
       <Box1 />
       <Box2 />
+      <Box3/>
     </div>
   );
 };
---
+++ components/box3.tsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+
+const Box3 = () => {
+  const boxVariant = {
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        type: "spring",
+      },
+    },
+  };
+
+  return (
+    <div className="h-full p-8 w-full bg-gray-100">
+      <motion.div
+        className="w-16 h-16 m-1 text-white text-xs flex items-center justify-center bg-orange-800 rounded-md"
+        variants={boxVariant}
+        initial="hidden"
+        animate="visible"
+      >
+        Hey!!!
+      </motion.div>
+    </div>
+  );
+};
+
+export default Box3;
